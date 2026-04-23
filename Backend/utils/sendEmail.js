c250fc25170de48e51a647236100f0d6b5fbecf4
Kalyan Chakraborty <kalyanchakraborty9064@gmail.com>
2026-04-23 20:11:18 +0530

    fix email verification link

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -10,30 +10,30 @@ const sendVerificationEmail = async (email, token) => {
   });
 
   const verificationLink =
-  `http://localhost:5173/verify-email/${token}`;
+    `${process.env.CLIENT_URL}/verify-email/${token}`;
 
   await transporter.sendMail({
     from: `"Boi Para" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "Verify Your Email",
     html: `
-  <h2>Email Verification</h2>
-  <p>Click below to verify your account:</p>
-  <a 
-    href="${verificationLink}"
-    style="
-      display:inline-block;
-      padding:12px 24px;
-      background:#16a34a;
-      color:white;
-      text-decoration:none;
-      border-radius:8px;
-      font-weight:bold;
-    "
-  >
-    Click Here to Verify
-  </a>
-`
+      <h2>Email Verification</h2>
+      <p>Click below to verify your account:</p>
+      <a 
+        href="${verificationLink}"
+        style="
+          display:inline-block;
+          padding:12px 24px;
+          background:#16a34a;
+          color:white;
+          text-decoration:none;
+          border-radius:8px;
+          font-weight:bold;
+        "
+      >
+        Click Here to Verify
+      </a>
+    `
   });
 };
 
