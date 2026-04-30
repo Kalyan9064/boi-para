eb7fde5de4b6e65cba891006ac951222187a588b
Kalyan Chakraborty <kalyanchakraborty9064@gmail.com>
2026-05-01 01:53:32 +0530

    fix email smtp port 587

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -5,12 +5,17 @@ const sendVerificationEmail = async (email, token) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // important
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
     console.log("📧 STEP 2: Transport created");
     console.log("EMAIL_USER:", process.env.EMAIL_USER);
